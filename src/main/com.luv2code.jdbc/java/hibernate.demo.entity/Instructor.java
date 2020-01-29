e4d95176ceeb84db3b498e0a75599498fd3c63ba
IsouthpawI <iiisouthpawiii@gmail.com>
2020-01-29 22:54:06 +0100

    add courses

--- src/main/com.luv2code.jdbc/java/hibernate.demo.entity/Instructor.java
+++ src/main/com.luv2code.jdbc/java/hibernate.demo.entity/Instructor.java
@@ -1,6 +1,8 @@
 package hibernate.demo.entity;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name="instructor")
@@ -37,6 +39,11 @@ public class Instructor {
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name="instructor_detail_id")
     private InstructorDetail instructorDetail;
+
+    @OneToMany(mappedBy = "instructor",
+                cascade={CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
+
+    private List<Course> courses;
 
     public Instructor(){
 
@@ -98,4 +105,25 @@ public class Instructor {
                 ", instructorDetail=" + instructorDetail +
                 '}';
     }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+
+    // add convenience methods for bi-directional relationship
+
+    public void add(Course tempCourse){
+        if(courses == null){
+            courses = new ArrayList<>();
+        }
+
+        courses.add(tempCourse);
+        tempCourse.setInstructor(this);
+
+    }
+
 }
