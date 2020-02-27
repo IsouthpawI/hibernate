748e2efb0b8e597cb2bff95e3b463295d5678748
IsouthpawI <iiisouthpawiii@gmail.com>
2020-02-27 14:21:22 +0100

    last changes

--- src/main/com.luv2code.jdbc/java/hibernate.demo.entity/Course.java
+++ src/main/com.luv2code.jdbc/java/hibernate.demo.entity/Course.java
@@ -1,6 +1,8 @@
 package hibernate.demo.entity;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name="Course")
@@ -27,6 +29,10 @@ public class Course {
     @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
     @JoinColumn(name = "instructor_id")
     private Instructor instructor;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "course_id")
+    private List<Review> reviews;
 
     public Course(){
 
@@ -58,6 +64,26 @@ public class Course {
 
     public void setInstructor(Instructor instructor) {
         this.instructor = instructor;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
+
+    // add a convenience method
+
+    public void addReview(Review theReview){
+
+        if(reviews == null){
+            reviews = new ArrayList<>();
+        }
+
+        reviews.add(theReview);
+
     }
 
     @Override
